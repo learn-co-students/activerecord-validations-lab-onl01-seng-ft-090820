816f1e619ffc7d53a3c6936cdeb6bb319b3e5862
LeahESc <leahemily1@gmail.com>
2020-11-03 15:45:23 -0800

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,6 +1,6 @@
 class TitleValidator < ActiveModel::Validator
     def validate(record)
-      unless record.title.start_with? ("Won't Believe")
+      unless record.title.include? ("Won't Believe")
         record.errors[:title] << "This title is not click bait-y enough"
       end
     end

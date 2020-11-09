143b1318ad8145b4e57ca111458d4c7f0c6343de
doosemavis <moosedavis2011@gmail.com>
2020-11-09 12:14:17 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 25 }
+    validates :content, length: { maximum: 200 }
+    validates :category, inclusion: 
 end
